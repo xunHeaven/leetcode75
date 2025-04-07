5588a38aa7716537255cd46555a46660079d089c
xun3203 <1343281941@qq.com>
2025-04-07 12:30:57 +0800

    完成第62题：790. 多米诺和托米诺平铺

---
+++ 62numTilings790.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    const int MOD=1e9+7;
+public:
+    int numTilings(int n) {
+        if(n==1) return 1;
+        long f[n+1];
+        f[0]=f[1]=1;
+        f[2]=2;
+        for(int i=3;i<=n;i++)
+        {
+            f[i]=(2*f[i-1]+f[i-3])%MOD;
+        }
+        return f[n];
+    }
+};
